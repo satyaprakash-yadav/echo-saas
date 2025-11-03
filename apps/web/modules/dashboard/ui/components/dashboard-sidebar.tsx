8cba9f407ede7e30b460ce0687462bc062171aac
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-11-03 21:51:37 +0530

    08 theme

--- apps/web/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ apps/web/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -116,6 +116,9 @@ export const DashboardSidebar = () => {
                                     <SidebarMenuButton
                                         asChild
                                         isActive={isActive(item.url)}
+                                        className={cn(
+                                            isActive(item.url) && "bg-gradient-to-b from-sidebar-primary to-[#0b63f3]! text-sidebar-primary-foreground! hover:to-[#0b63f3]/90!"
+                                        )}
                                         tooltip={item.title}
                                     >
                                         <Link href={item.url}>
@@ -139,6 +142,9 @@ export const DashboardSidebar = () => {
                                     <SidebarMenuButton
                                         asChild
                                         isActive={isActive(item.url)}
+                                        className={cn(
+                                            isActive(item.url) && "bg-gradient-to-b from-sidebar-primary to-[#0b63f3]! text-sidebar-primary-foreground! hover:to-[#0b63f3]/90!"
+                                        )}
                                         tooltip={item.title}
                                     >
                                         <Link href={item.url}>
@@ -162,6 +168,9 @@ export const DashboardSidebar = () => {
                                     <SidebarMenuButton
                                         asChild
                                         isActive={isActive(item.url)}
+                                        className={cn(
+                                            isActive(item.url) && "bg-gradient-to-b from-sidebar-primary to-[#0b63f3]! text-sidebar-primary-foreground! hover:to-[#0b63f3]/90!"
+                                        )}
                                         tooltip={item.title}
                                     >
                                         <Link href={item.url}>
