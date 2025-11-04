f3f39d9ec4fac411c6d5f6c2933679b7894e7865
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-11-04 20:05:49 +0530

    12: widget loading

--- apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
@@ -17,11 +17,18 @@ import { formSchema } from "../../schemas";
 import { useMutation } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Doc } from "@workspace/backend/_generated/dataModel";
+import { useAtomValue, useSetAtom } from "jotai";
+import { contactSessionIdAtomFamily, organizationIdAtom } from "../../atoms/widget-atoms";
 
 // Temporary test organizationId, before we add state management
 const organizationId = "123";
 
 export const WidgetAuthScreen = () => {
+    const organizationId = useAtomValue(organizationIdAtom);
+    const setContactSessionId = useSetAtom(
+        contactSessionIdAtomFamily(organizationId || "")
+    );
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -57,7 +64,7 @@ export const WidgetAuthScreen = () => {
             metadata,
         });
 
-        console.log({ contactSessionId });
+        setContactSessionId(contactSessionId);
     };
 
     return (
