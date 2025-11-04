b961a69f9672e35ba29535a2e73e66f335073e48
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-11-04 11:52:42 +0530

    10: widget session

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -2,6 +2,7 @@
 
 import { WidgetFooter } from "../components/widget-footer";
 import { WidgetHeader } from "../components/widget-header";
+import { WidgetAuthScreen } from "../screens/widget-auth-screen";
 
 interface Props {
     organizationId: string;
@@ -13,20 +14,8 @@ export const WidgetView = ({
     return (
         // TODO: Confirm whether or not "min-h-screen" and "min-w-screen" is needed
         <div className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-            <WidgetHeader>
-                <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
-                    <p className="text-3xl">
-                        Hi there! 👋
-                    </p>
-                    <p className="text-lg">
-                        How can we help you today?
-                    </p>
-                </div>
-            </WidgetHeader>
-            <div className="flex flex-1">
-                Widget View: {organizationId}
-            </div>
-            <WidgetFooter />
+            <WidgetAuthScreen />
+            {/* <WidgetFooter /> */}
         </div>
     );
 };
