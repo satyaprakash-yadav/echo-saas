4818247ddc50bf485a56a43a087a3789fe2bd3e2
“satyaprakash-yadav” <satyay9798@gmail.com>
2026-02-01 16:51:02 +0530

    31: subscriptions

--- apps/web/app/(dashboard)/billing/page.tsx
+++ apps/web/app/(dashboard)/billing/page.tsx
@@ -1,8 +1,8 @@
+import { BillingView } from "@/modules/billing/ui/views/billing-view";
+
 const Page = () => {
     return ( 
-        <div>
-            Plans & Billing!
-        </div>
+        <BillingView />
      );
 }
  
--- apps/web/app/(dashboard)/customization/page.tsx
+++ apps/web/app/(dashboard)/customization/page.tsx
@@ -1,9 +1,20 @@
+import { PremiumFeatureOverlay } from "@/modules/billing/ui/components/premium-feature-overlay";
 import { CustomizationView } from "@/modules/customization/ui/views/customization-view";
+import { Protect } from "@clerk/nextjs";
 
 const Page = () => {
-    return ( 
-        <CustomizationView />
-     );
+    return (
+        <Protect
+            condition={(has) => has({ plan: "pro" })}
+            fallback={
+                <PremiumFeatureOverlay>
+                    <CustomizationView />
+                </PremiumFeatureOverlay>
+            }
+        >
+            <CustomizationView />
+        </Protect>
+    );
 }
- 
+
 export default Page;
