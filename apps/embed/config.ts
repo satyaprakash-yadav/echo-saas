8fff84f74357c20bc06d345be10e55981111e307
“satyaprakash-yadav” <satyay9798@gmail.com>
2026-02-10 22:34:24 +0530

    34: embed script

---
+++ apps/embed/config.ts
@@ -0,0 +1,5 @@
+export const EMBED_CONFIG = {
+    WIDGET_URL: import.meta.env.VITE_WIDGET_URL || "http://localhost:3001",
+    DEFAULT_ORG_ID: "org_34muotUKbaZl8jAIxwvDH7i0Jb6",
+    DEFAULT_POSITION: "bottom-right" as const,
+}
